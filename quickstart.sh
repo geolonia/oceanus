b777b920c65ece0035add197e48e860299c58f2a
takamotobiz <takamotobiz@takamoto.biz>
2020-12-24 06:57:27 +0900

    バグ修正

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# = 2 ]; then
     docker run -it --rm --name tile-builder -v $1:/data geolonia/tile-builder /app/tile-builder
-    docker run -d -it --name tb-server -v $1:/data klokantech/tileserver-gl -c $1/config.json -p $2
+    docker run -d -it --name tb-server -v $1:/data -p $2:80 klokantech/tileserver-gl -c $1/config.json
 else
     echo "引数１にデータディレクトリ、引数２にtileserverのポートを指定してください。"
 fi
