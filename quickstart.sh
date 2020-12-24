b34ede88f9a1f4739fb6bee1d8f197de9b2c65da
takamotobiz <takamotobiz@takamoto.biz>
2020-12-24 22:01:46 +0900

    tile作成とtileserver起動を分離

--- quickstart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $# = 2 ]; then
-    docker run -it --rm --name tile-builder -v $1:/data geolonia/tile-builder /app/tile-builder
-    docker run -d -it --name tb-server -v $1:/data -p $2:80 klokantech/tileserver-gl -c $1/config.json
-else
-    echo "引数１にデータディレクトリ、引数２にtileserverのポートを指定してください。"
-fi
---
+++ runtilesv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ $# = 2 ]; then
+    docker run -it --rm --name tile-builder -v $1:/data geolonia/tile-builder /app/settilesvenv
+    docker container stop tilesv
+    docker container rm tilesv
+    docker run -d -it --name tilesv -v $1:/data -p $2:80 klokantech/tileserver-gl -c $1/config.json
+else
+    echo "引数１にデータディレクトリ、引数２にtileserverのポート番号を指定してください。"
+fi
