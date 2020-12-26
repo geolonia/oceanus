5974a779d7ebbea2bca3b1d665d151a54b0a4446
takamotobiz <takamotobiz@takamoto.biz>
2020-12-26 12:14:40 +0900

    build時にyamlを反映させる対処

--- tile-builder.sh
+++ tile-builder.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ $# = 1 ]; then
+    copy ./tile-builder.yaml $1
     docker run -it --rm --name tile-builder -v $1:/data geolonia/tile-builder /app/tile-builder
 else
     echo "引数１にデータディレクトリを指定してください。"
