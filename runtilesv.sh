79d6bfe84ff6bbd5a8c0938d001e230893b8cc67
takamotobiz <takamotobiz@takamoto.biz>
2020-12-26 11:35:53 +0900

    フォント配信失敗対応

--- runtilesv.sh
+++ runtilesv.sh
@@ -3,7 +3,7 @@ if [ $# = 2 ]; then
     docker run -it --rm --name tile-builder -v $1:/data geolonia/tile-builder /app/settilesvenv
     docker container stop tilesv
     docker container rm tilesv
-    docker run -d -it --name tilesv -v $1:/data -p $2:80 klokantech/tileserver-gl -c $1/config.json
+    docker run -d -it --name tilesv -v $1:/data -p $2:80 klokantech/tileserver-gl -c /data/config.json
 else
     echo "引数１にデータディレクトリ、引数２にtileserverのポート番号を指定してください。"
 fi
