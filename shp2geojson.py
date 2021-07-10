801bd8678de98f33cc278a56ee78546b20f279f1
takamotobiz <keiji.takamoto@takamoto.biz>
2021-07-10 15:15:42 +0900

    merge-oceanus-internal

--- shp2geojson.py
+++ shp2geojson.py
@@ -2,16 +2,16 @@
 Convert ESRI Shapefile to GeoJSON.
 """
 import yaml
+import csv
 import fiona
 import datetime as dt
-from shapely.geometry import shape,Polygon
+from shapely.geometry import shape,Polygon,MultiPolygon
 from shapely.ops import polylabel
-import json
 from GJWriter import GJWriter
 
 # Open setting file.
-with open('/app/shp2geojson.yaml' ,'r') as yml:
-# with open('./shp2geojson.yaml' ,'r') as yml:
+with open('./shp2geojson.yaml' ,'r') as yml:
+    # with open('./shp2geojson.yaml' ,'r') as yml:
     config = yaml.load(yml, Loader=yaml.SafeLoader)
 
 # Create instance of GJWriter
@@ -20,19 +20,58 @@ gjWriter = GJWriter(config['outputfile'])
 basedir = config['basedir']
 layers = config['layers']
 
+# Get Japan area( only 1 polygon )
+with fiona.open(basedir+config['japanarea']) as shapes:
+    japanarea = shape(next(shapes)['geometry'])
+
 # Get layer information
 for layer in layers:
     # Get file information
     for filename in layer['file']:
+
         # Print start message
         print('Start:['+str(dt.datetime.now())+']'+filename['fname'])
+
+        # Create replace dictionary
+        if 'replacecsv' in filename:
+            with open(basedir+filename['replacecsv'], encoding='utf-8-sig') as fd:
+                dr = csv.reader(fd)
+                citydict={}
+                for r in dr:
+                    citydict.update({r[0]:r[1]})
+            replacecsv = True
+        else:
+            replacecsv = False
+
         # Open the shapefile
         elements = fiona.open(basedir+filename['fname'])
+
+        # Get property filter condition
+        if 'condition' in filename:
+            condition = filename['condition'].split('=')
+            keylist = condition[1].split('/')
+            conddic = {key: '' for key in keylist}
+
         # Get elements
         for element in elements:
+            # Create CSV to Geometry
+            if 'filetype' in filename:
+                csvelement={"type":"Point"}
+                csvelement.update({"coordinates":(float(element['properties']['x']) , float(element['properties']['y']))})
+                element['geometry']=csvelement
+
+            # property filter( equal condition only )
+            if 'condition' in filename:
+                if element['properties'][condition[0]] not in conddic:
+                    # Skip output GeoJSON
+                    gjWriter.Clear()
+                    continue
+
             # Ignore Null Shapes
             if element['geometry']==None:
+                gjWriter.Clear()
                 continue
+
             # Set "Property" member
             if 'class' in filename:
                 gjWriter.setProperty('class' ,filename['class'])
@@ -42,8 +81,46 @@ for layer in layers:
                 gjWriter.setProperty('admin_level' ,filename['admin_level'])
             if 'name' in filename:
                 if element['properties'][filename['name']]!=None:
-                    # Set river name to property
-                    gjWriter.setProperty('name' ,element['properties'][filename['name']])
+                    if replacecsv:
+                        # Check Japan pref-capital list
+                        if element['properties'][filename['name']] in citydict:
+                            if citydict[element['properties'][filename['name']]]=='':
+                                # Skip output GeoJSON
+                                gjWriter.Clear()
+                                continue
+                            else:
+                                # Replace pref-capital name
+                                gjWriter.setProperty('name' ,citydict[element['properties'][filename['name']]])
+                        else:
+                            # Set name to property
+                            gjWriter.setProperty('name' ,element['properties'][filename['name']])
+                    else:
+                        # Set name to property
+                        gjWriter.setProperty('name' ,element['properties'][filename['name']])
+                else:
+                    # Skip output GeoJSON
+                    gjWriter.Clear()
+                    continue
+                    
+            # Set Scalerank
+            if 'scalerank' in filename:
+                if element['properties']['scalerank'] > filename['scalerank']:
+                    gjWriter.Clear()
+                    continue
+
+            # Set Japan territory
+            if 'jflag' in filename:
+                if japanarea.intersection(shape(element['geometry'])):
+                    gjWriter.setProperty('jflag' ,'japan')
+                elif filename['jflag']=='erase':
+                    # Issue#17
+                    gjWriter.Clear()
+                    continue
+
+            # Set pref-capital property
+            if 'pref-capital' in filename:
+                gjWriter.setProperty('pref-capital' ,'true')
+
             # Set "Tippecanoe" member
             gjWriter.setTippecanoe('layer' ,layer['layer'])
             if 'minzoom' in filename:
@@ -56,6 +133,7 @@ for layer in layers:
                     print('Disputed border:[' + element['properties']['note'] + ']')
                     gjWriter.Clear()
                     continue
+
             # Set "Geometry" member
             if 'attr' in filename:
                 # Issue#21(Do not mark "None" on the map)
@@ -90,8 +168,22 @@ for layer in layers:
                     # Set "Geometry" member
                     gjWriter.setGeometry(geometry)
             else:
-                # Write "Geometry" member(Other than attribute label)
-                gjWriter.setGeometry(element['geometry'])
+                # Simplify Geometry
+                if 'simplify' in filename:
+                    if element['geometry']['type']=='Polygon':
+                        polygon = Polygon(shape(element['geometry']))
+                        geometry = "{\"type\":\"Polygon\",\"coordinates\":["
+                        geometry += str(polygon.simplify(config['simplifyparam'])).replace("(","[").replace(")","]").replace("\'","\"").replace("POLYGON " ,"").replace(", " ,"],[").replace(" " ,",")
+                        geometry += "]}"
+                    else:
+                        polygon = MultiPolygon(list(shape(element['geometry'])))
+                        geometry = "{\"type\":\"MultiPolygon\",\"coordinates\":["
+                        geometry += str(polygon.simplify(config['simplifyparam'])).replace("(","[").replace(")","]").replace("\'","\"").replace("MULTIPOLYGON " ,"").replace(", " ,"],[").replace(" " ,",")
+                        geometry += "]}"
+                    # Set "Geometry" member
+                    gjWriter.setGeometry(geometry)
+                else:
+                    gjWriter.setGeometry(element['geometry'])
             # Write "Geometry" member
             gjWriter.Write()
             
