e96eb11b1c36efbe0a7c4933026baaabb98740dd
takamotobiz <keiji.takamoto@takamoto.biz>
2021-07-11 15:09:18 +0900

    スクリプトの無駄を修正

--- oceanus.sh
+++ oceanus.sh
@@ -26,6 +26,5 @@ done
 
 echo "Building Oceanus Docker Image.Tag name is geolonia/oceanus."
 docker build -t geolonia/oceanus .
-cp ./shp2geojson.yaml $DIR
 echo "docker run -it --rm --name oceanus -v ${DIR}:/data geolonia/oceanus /app/ne2mbtiles"
 docker run -it --rm --name oceanus -v $DIR:/data geolonia/oceanus /app/ne2mbtiles
